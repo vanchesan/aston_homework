389fe1fa0a859acbac6020616c36de61a37c292e
vanchesan <vanchesan903@gmail.com>
2025-04-14 20:32:06 +0300

    implemented validation User

--- src/main/java/org/example/Enity/User.java
+++ src/main/java/org/example/Enity/User.java
@@ -1,11 +1,10 @@
 package org.example.Enity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Check;
+import jakarta.validation.constraints.*;
+
 
 
 import java.time.LocalDate;
@@ -19,7 +18,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank (message = "Имя не может быть пустым")
+    @Size(min = 2, max = 50, message = "Имя может содержить от 2 до 50 симвлов")
+    @Column(name = "name", nullable = false)
     private String name;
+    @Email(message = "Неккоретный email1")
     @Column (name ="email", unique = true)
     private String email;
     @Check(constraints = "age >= 0 AND age <=120")
--- src/main/java/org/example/repository/UserRepoImpl.java
+++ src/main/java/org/example/repository/UserRepoImpl.java
@@ -2,12 +2,14 @@ package org.example.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.validation.*;
 import lombok.RequiredArgsConstructor;
 import org.example.Enity.User;
 import org.hibernate.SessionFactory;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 @RequiredArgsConstructor
 public class UserRepoImpl implements UserRepo {
@@ -16,8 +18,21 @@ public class UserRepoImpl implements UserRepo {
 
     @Override
     public void save(User entity) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Set<ConstraintViolation<User>> violations = validator.validate(entity);
+        if (!violations.isEmpty()) {
+            StringBuilder errors = new StringBuilder("Ошибки валидации:\n");
+            for (ConstraintViolation<User> violation : violations) {
+                errors.append("- ").append(violation.getMessage()).append("\n");
+            }
+            throw new IllegalArgumentException(errors.toString());
+        }
+
         EntityManager em = sessionFactory.createEntityManager();
         EntityTransaction transaction = em.getTransaction();
+
         try {
             transaction.begin();
             em.persist(entity);
